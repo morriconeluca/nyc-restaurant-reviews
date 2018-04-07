9c6072a84ac6a0213b6dac117d5d1243a942f005
Luca Morricone <morricone.luca@outlook.com>
2018-04-07 14:38:38 +0200

    refactor: Replace h1 with h2 in createRestaurantHTML and add article

--- js/main.js
+++ js/main.js
@@ -137,31 +137,34 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
+  const article = document.createElement('article');
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  li.append(image);
+  article.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  article.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  article.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  article.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  article.append(more);
 
-  return li
-}
+  li.append(article);
+
+  return li;
+};
 
 /**
  * Add markers for current restaurants to the map.
